b1081366157a36ea9070142fbe72c31b251765c2
Robin Roth <robin.roth1@students.fhnw.ch>
2025-08-28 15:16:11 +0200

    add generator function for fibonacci numbers

--- src/pypl/algorithms/fib.py
+++ src/pypl/algorithms/fib.py
@@ -2,6 +2,7 @@
 
 import timeit
 from functools import lru_cache
+from typing import Iterator
 
 def fib(n: int) -> int:
     """Get the n-th element of the Fibonacci sequence. (naive recursive
@@ -43,8 +44,20 @@ def fib_iter(n: int) -> int:
     return current
 
 
+def generate_fib(n: int) -> Iterator[int]:
+    """Genrate all Fibonacci numbers up to and including the n-th
+    number."""
+    yield 0
+    if n > 0: yield 1
+    prev: int = 0
+    current: int = 1
+    for _ in range(1, n):
+        current, prev = current + prev, current
+        yield current
+
+
 if __name__ == "__main__":
-    n = 50
+    n = 30
     assert fib_memo(n) == fib_auto_memo(n) == fib_iter(n), ("Not all functions "
         "return the same result")
     
@@ -57,3 +70,6 @@ if __name__ == "__main__":
     print(f"Memoization implementation: {t_memo:>19.7f}s")
     print(f"Automatic Memoization implementation: {t_auto_memo:>2.7f}s")
     print(f"Iterative implementation: {t_iter:>21.7f}s")
+
+    fib_to_n = [i for i in generate_fib(3)]
+    print(fib_to_n)
